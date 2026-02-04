41219df5243d91e046478a7e654e1b2a3d15ba80
jxom <7336481+jxom@users.noreply.github.com>
2026-02-04 14:20:23 +1100

    fix: usage

--- src/mpay.server.ts
+++ src/mpay.server.ts
@@ -1,24 +1,8 @@
 import { env } from "cloudflare:workers";
 import { Mpay, tempo } from "mpay/server";
-import { createClient, http } from "viem";
-import { tempoModerato } from "viem/chains";
-
-const chain = (() => {
-	switch (env.CHAIN) {
-		case "moderato":
-			return tempoModerato;
-		default:
-			throw new Error(`Unsupported chain: ${env.CHAIN}`);
-	}
-})();
-
-const client = createClient({
-	chain,
-	transport: http(),
-});
 
 export const mpay = Mpay.create({
-	method: tempo({ client }),
+	method: tempo(),
 	realm: "mpp.dev",
 	secretKey: env.SECRET_KEY! ?? "top-secret",
 });
